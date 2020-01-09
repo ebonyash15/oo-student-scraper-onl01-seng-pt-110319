14888fc2f91535a11327290475da73f3cc2b238c
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-09 01:02:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,22 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
-
 class Scraper
-
+  @index_array=[]
   def self.scrape_index_page(index_url)
-    
+    html=open(index_url)
+    doc=Nokogiri::HTML(html)
+    elements=doc.css(".student-card")
+    hash={}
+    elements.collect do |element|
+  #     binding.pry
+      hash[:name]=element.css("h4.student-name").text
+      hash[:location]=element.css(".student-location").text
+      hash[:profile_url]=element.css("a").attribute("href").value
+      @index_array<<hash
+    end
   end
-
   def self.scrape_profile_page(profile_url)
-    
+
   end
-
 end
-
--- lib/student.rb
+++ lib/student.rb
@@ -1,23 +1,16 @@
 class Student
-
-  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url 
-
+  attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
   @@all = []
-
   def initialize(student_hash)
-    
-  end
 
+  end
   def self.create_from_collection(students_array)
-    
-  end
 
+  end
   def add_student_attributes(attributes_hash)
-    
-  end
 
+  end
   def self.all
-    
+
   end
 end
-
