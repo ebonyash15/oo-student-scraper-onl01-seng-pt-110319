58d8c67d569c4e45e6588a7b3140d3cc8ad2c4af
Ebony Sharrock <ebonysharrock@gmail.com>
2020-01-09 01:08:10 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,6 @@ class Student
   attr_accessor :name, :location, :twitter, :linkedin, :github, :blog, :profile_quote, :bio, :profile_url
   @@all = []
   def initialize(student_hash)
-    binding.pry
      hash[:name]=Scraper.element.css("h4.student-name").text
      hash[:location]=Scraper.element.css(".student-location").text
      hash[:profile_url]=Scraper.element.css("a").attribute("href").value
